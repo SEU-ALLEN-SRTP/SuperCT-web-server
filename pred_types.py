8a3d4699d031104d4f8433fddb635bbc0661b63a
Odehan <47688877+zzhmark@users.noreply.github.com>
2021-06-17 19:12:04 +0800

    streamlit ok

--- pred_types.py
+++ pred_types.py
@@ -1,105 +1,126 @@
-#!/usr/bin/env python
-# coding: utf-8
-import sys,getopt
-
-usage='''Usage: python pred_types.py -i <input_dge.csv> -o <cell_type_output.csv> -p -m <model.h5> -s <human or mouse> -t <id2type.csv>
-	or: python pred_types.py --input_file=<input_dge.csv> --output_file=<cell_type_output.csv> --plot_hist --model=<model.h5> --species=<human or mouse> --types=<id2type.csv>
-	'''
-try:
-	opts,args = getopt.getopt(sys.argv[1:], "hpi:o:m:s:t:", ["help",'plot','input_file=','output_file=','model=','species=','types='])
-except getopt.GetoptError:
-	print('Error\n',usage)
-	sys.exit(2)
-output_file='cell_type_output.csv'
-input_file=''
-model_file=''
-plot_hist=False
-species='human'
-types=''
-for opt,arg in opts:
-	if opt in ("-h", "--help"):
-		print(usage)
-		sys.exit()
-	elif opt in ("-i", "--input_file"):
-		input_file = arg
-	elif opt in ("-o", "--output_file"):
-		output_file = arg
-	elif opt in ("-m", "--model"):
-		model_file = arg
-	elif opt in ('-p','--plot_hist'):
-		plot_hist=True
-	elif opt in ('-s','--species'):
-		species = arg
-	elif opt in ('-t','--types'):
-		types=arg
-if len(input_file)==0:
-	print('expression file must be supplied!')
-	print(usage)
-	sys.exit()
-if len(model_file)==0:
-	print('model file must be supplied!')
-	print(usage)
-	sys.exit()
-#make sure these module installed
 import numpy as np
 import pandas as pd
 from keras.models import load_model
+import streamlit as st
 import matplotlib.pyplot as plt
+import base64
+import time
+import plotly.express as px
+from sklearn.manifold import TSNE
 
-def load_genes(genes_file,species):
-	if species=='human':
-		genes_used = pd.read_csv(genes_file,usecols=[0],header = None)
-	elif species=='mouse':
-		genes_used = pd.read_csv(genes_file,usecols=[1],header = None)
-	genes_used = genes_used.values.tolist()
-	for i,g in enumerate(genes_used):
-		genes_used[i]=g[0]
-	return genes_used
-def load_data(file_name,genes_used):
-	data = pd.read_csv(file_name, header = 0, index_col = 0)
-	data = data[~data.index.duplicated(keep='first')]
-	genes_valid=np.intersect1d(data.index,genes_used)
-	if len(genes_valid)==len(genes_used):
-		data=data.loc[genes_used,:]
-	elif len(genes_valid) < len(genes_used):
-		genes_not_found=np.setdiff1d(genes_used,genes_valid)
-		nonezero_df=pd.DataFrame(np.zeros((len(genes_not_found),data.shape[1])).astype('int64'))
-		nonezero_df.columns=data.columns
-		data=pd.concat([data,nonezero_df])
-		data=data.loc[genes_used,:]
-		print('Warning:',genes_not_found,'not found in your expression file,please check them.but the prediction will go on by setting these genes with zero count.')
-	return data
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+
+def load_genes(genes_file, species):
+    genes_used = pd.read_csv(genes_file, usecols=[0 if species == 'human' else 1], header=None).values.tolist()
+    for i, g in enumerate(genes_used):
+        genes_used[i] = g[0]
+    return genes_used
+
+
+def load_data(buffer, genes_used):
+    data = pd.read_csv(buffer, header=0, index_col=0)
+    data = data[~data.index.duplicated(keep='first')]
+    genes_valid = np.intersect1d(data.index, genes_used)
+    if len(genes_valid) == len(genes_used):
+        data = data.loc[genes_used, :]
+    elif len(genes_valid) < len(genes_used):
+        genes_not_found = np.setdiff1d(genes_used, genes_valid)
+        nonezero_df = pd.DataFrame(np.zeros((len(genes_not_found), data.shape[1])).astype('int64'))
+        nonezero_df.columns = data.columns
+        data = pd.concat([data, nonezero_df])
+        data = data.loc[genes_used, :]
+        print('Warning:', genes_not_found, ''' not found in your expression file,please check them. 
+        But the prediction will go on by setting these genes with zero count.''')
+    return data
+
+
 def load_cell_types(types):
-	cell_types={}
-	all_types=pd.read_csv(types)
-	for i,each in enumerate(all_types['celltype']):
-		cell_types[i]=each
-	return cell_types
-def predict_type(model,data,cell_types):
-	data = data > 0
-	data = data.astype(np.uint8)
-	result = model.predict(data.values.T)
-	result_df = pd.DataFrame(result)
-	result_df.columns = list(cell_types.values())
-	cell_type_pred = [cell_types[i] for i in list(np.argmax(result, axis = 1))]
-	result_df["pred_type"] = cell_type_pred
-	return result_df
+    cell_types = {}
+    all_types = pd.read_csv(types)
+    for i, each in enumerate(all_types['celltype']):
+        cell_types[i] = each
+    return cell_types
 
-model=load_model(model_file)
-genes_used=load_genes('inthomgenes.csv',species)
-data=load_data(input_file,genes_used)
-cell_types=load_cell_types(types)
 
-result_df=predict_type(model,data,cell_types)
-result_out=pd.DataFrame({'cell_id': data.columns,'pred_type':result_df['pred_type']})
-result_out.to_csv(output_file,index=False)
+def predict_type(model, data, cell_types):
+    data = data > 0
+    data = data.astype(np.uint8)
+    result = model.predict(data.values.T)
+    result_df = pd.DataFrame(result)
+    result_df.columns = list(cell_types.values())
+    cell_type_pred = [cell_types[i] for i in list(np.argmax(result, axis=1))]
+    result_df["pred_type"] = cell_type_pred
+    return result_df
 
-if plot_hist:
-	d={}
-	res_list=list(result_out['pred_type'])
-	for i in res_list:
-		d.update({i:res_list.count(i)})
-	plt.bar(d.keys(),d.values())
-	plt.title('Frequency of predicted cell types')
-	plt.xticks(rotation=90)
-	plt.savefig('predicted_types.pdf',bbox_inches='tight')
+
+def downloader(text, fname):
+    b64 = base64.b64encode(text.encode()).decode()
+    label = fname.replace("_", r"\_")
+    href = f'<a href="data:file/csv;base64,{b64}" download="{fname}">{label}</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+
+def plot_histogram(res):
+    st.subheader('Cell type histogram')
+    d = {}
+    res_list = list(res['pred_type'])
+    for i in res_list:
+        d.update({i: res_list.count(i)})
+    plt.bar(d.keys(), d.values())
+    plt.title('Frequency of predicted cell types')
+    plt.xticks(rotation=90)
+    st.pyplot(plt, bbox_inches='tight')
+
+
+def plot_tsne(df, res):
+    features = df.transpose()
+    tsne = TSNE(n_components=3, random_state=0)
+    projections = tsne.fit_transform(features)
+
+    fig = px.scatter_3d(
+        projections, x=0, y=1, z=2,
+        color=res.pred_type, labels={'color': 'pred_type'}
+    )
+    fig.update_traces(marker_size=5)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+def main():
+    st.title('SuperCT Web Server')
+    st.text('Mapping your scRNA-seq data to cell types, with our neural network model.')
+
+    # input params
+    st.header('Options')
+    model = load_model('models/' + st.selectbox('Select a trained neural network model', ['v1_model.h5']))
+    cell_types = load_cell_types('models/' + st.selectbox('Select a trained neural network model', ['v1_id2type.csv']))
+    genes_used = load_genes('inthomgenes.csv', st.selectbox('Select the species of your data', ['human', 'mouse']))
+    plot_hist = st.checkbox('Plot Cell Type Histogram', True)
+    mapping_viewer = st.checkbox('View mapping results in tSNE', True)
+    # input data
+    st.header('Data')
+    upload = st.file_uploader('Upload your DGE table', ['csv'],
+                              help='differential gene expression table (csv), binary')
+    if upload is not None:
+        data = load_data(upload, genes_used)
+        st.write('First 100 rows and columns of your uploaded table...')
+        st.dataframe(data.iloc[0:99, 0:99])
+        if st.button('Run Prediction'):
+            st.header('Results')
+            result_df = predict_type(model, data, cell_types)
+            result_out = pd.DataFrame({'cell_id': data.columns, 'pred_type': result_df['pred_type']})
+            st.subheader('Predictions (first 100 rows)')
+            st.dataframe(result_out.iloc[0:99])
+            if plot_hist:
+                plot_histogram(result_out)
+            if mapping_viewer:
+                plot_tsne(data, result_out)
+            if mapping_viewer:
+                st.subheader('Mapping Viewer')
+
+            st.subheader('Download')
+            downloader(result_out.to_csv(index=False), "cell_type_prediction_{}.csv".format(timestr))
+
+
+if __name__ == '__main__':
+    main()
